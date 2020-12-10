8d13e6ea0f1c765652547b8730a4f3d71920dd86
github username <github email address>
2020-12-10 14:15:02 +0000

    pared down project

--- react-practice-0111/src/App.js
+++ react-practice-0111/src/App.js
@@ -1,11 +1,7 @@
 import './App.css'
 
 function App () {
-  return (
-    <div className='App'>
-      <header className='App-header' />
-    </div>
-  )
+  return <div className='App'>'cool'</div>
 }
 
 export default App
--- react-practice-0111/src/index.js
+++ react-practice-0111/src/index.js
@@ -4,9 +4,11 @@ import './index.css'
 import App from './App'
 
 ReactDOM.render(
+  // this puts the /App file on the page - what to render
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>, // is strict mode new?
+  // where to render-- on the document at the 'root' id
   document.getElementById('root')
 )
 
